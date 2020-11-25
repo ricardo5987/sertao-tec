5b9baacd45df07891ce7f8543650c5bb74bbb8f1
ricardo5987 <ricardo.e5987@outlook.com>
2020-11-25 14:46:35 -0300

    projeto inicial com semantic ui incluso

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import 'semantic-ui-css/semantic.min.css'
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import {Button } from 'semantic-ui-react'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
+      <Button>ClickMe!!!</Button>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
